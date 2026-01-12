abccbf3ebf287aa72cc2638a143eeab164fcd7ee
Danil Nikolaev <lischenkodev@gmail.com>
2026-01-12 16:09:22 +0300

    updates for bun and docker update libs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import {defineConfig} from "drizzle-kit";
-import {Environment} from "./src/common/environment";
 
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
     dialect: "sqlite",
     dbCredentials: {
-        url: Environment.DB_PATH,
+        url: process.env.DB_FILE_NAME,
     },
 });
